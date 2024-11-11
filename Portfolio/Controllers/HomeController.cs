4bee864c9707cc90ede25635d448222a05ea7f46
Breno Ilan <168735619+brenoilann@users.noreply.github.com>
2024-11-11 01:06:33 +0000

    y
    y

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string lang = "en")
+        {
+            ViewBag.SelectedLang = lang;
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
